9b25a5bb240b5aabf5eb767965d240d2553efd24
Alejandro Penaloza <43787952+apenaloza7@users.noreply.github.com>
2020-04-19 00:43:31 -0400

    Created database methods

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
 
 """
 Main file for my CRUD application for 471
@@ -11,12 +13,40 @@ Last updated - 4/18/2020
 
 # Creating the app using Flask
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
+
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': 'localhost:8889',
+  'database': 'inventory',
+  'raise_on_warnings': True,
+}
+
+db = mysql.connector.connect(**config)
+
+# db = SQLAlchemy(app)
 
 # Create router for the landing page
 @app.route('/')
 def Index():
     return render_template("index.html")
 
+# TODO: follow mysql.connector guide to udpate this method
+@app.route('/insert', methods = ['POST'])
+def insertEmployee():
+    if request.method == 'POST':
+        SSN = request.form['SSN']
+        DOB = request.form['DOB']
+        Fname = request.form['Fname']
+        Minit = request.form['Minit']
+        Lname = request.form['Lname']
+        Address = request.form['Address']
+
+    my_employee = 
+
+    return redirect(url_for('Index'))
+        
 if __name__ == "__main__":
     app.run(debug = True)
 
